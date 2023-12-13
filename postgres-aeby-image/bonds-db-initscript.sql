8311ff3a37ab3e1afe28843363842db87ae21b44
stefan aeby <aeby_21@hotmail.com>
2023-12-13 23:21:08 +0100

    everything changed to postgres data

--- postgres-aeby-image/bonds-db-initscript.sql
+++ postgres-aeby-image/bonds-db-initscript.sql
@@ -1,5 +1,28 @@
-CREATE TABLE time_series_value (
+CREATE TABLE bonds_table (
+
     id SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL,
-    value NUMERIC
+    emittent VARCHAR(128) NULL,
+    parent_id VARCHAR(128) NULL,
+    identifier VARCHAR(128) NULL,
+    sust_linked SMALLINT NULL,
+    ticker VARCHAR (10) NULL,
+    social_bond SMALLINT NULL,
+    green_instrument SMALLINT NULL,
+    sitzland VARCHAR(3) NULL,
+    ausg_menge REAL NULL,
+    maturity REAL NULL,
+    emissionsdatum Date NULL,
+    bics_level_1 VARCHAR(128) NULL,
+    kpn REAL NULL,
+    faelligkeit DATE NULL,
+    faell_typ VARCHAR(128) NULL,
+    waehrung VARCHAR(10) NULL,
+    total_assets BIGINT NULL,
+    tobinsq REAL NULL,
+    roa REAL NULL,
+    roe REAL NULL,
+    lt_leverage REAL NULL,
+    trbc_es VARCHAR(128) NULL,
+    cf_to_asset REAL NULL
+
 );
