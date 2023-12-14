54a50558282aa543f9e34b0ab099e6939169b81b
aeby21 <135545245+aeby21@users.noreply.github.com>
2023-12-15 00:11:58 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-
-import pandas as pd
-import pyarrow
-
-bonds_data_df = pd.read_excel("Data/Daten Sustainable Bonds Bloomberg.xlsx",sheet_name="Bloomberg")
-bonds_data_df.to_feather("Data/green_bonds.arrow")
-print(bonds_data_df.iloc[0])
